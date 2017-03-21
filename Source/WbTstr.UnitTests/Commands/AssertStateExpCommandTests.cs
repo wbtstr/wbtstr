f7f59337bb6a74be8cd7c72417aa0c0ea88cea9f
Onno Valkering <ovalkering@mirabeau.nl>
2017-03-21 16:49:26 +0100

    Added unit tests for AssertStateExpCommand

---
+++ Source/WbTstr.UnitTests/Commands/AssertStateExpCommandTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using WbTstr.Commands;
+using WbTstr.WebDrivers.Constants;
+
+namespace WbTstr.UnitTests.Commands
+{
+    [TestFixture]
+    public class AssertStateExpCommandTests
+    {
+        private const string DefaultPropertyValue = "WbTstr 4 Life";
+
+        [TestCase]
+        public void Constructor_TypicalPropertyKeyValue_DoesNotThrow()
+        {
+            // Arrange
+
+            // Act
+            TestDelegate action = () => new AssertStateExpCommand(x => x.Title == DefaultPropertyValue);
+
+            // Assert
+            Assert.DoesNotThrow(action);
+        }
+
+        [TestCase]
+        public void Constructor_UninitializedPropertyValue_DoesNotThrow()
+        {
+            // Arrange
+            string propertyValue = null;
+
+            // Act
+            TestDelegate action = () => new AssertStateExpCommand(x => x.Title == propertyValue);
+
+            // Assert
+            Assert.DoesNotThrow(action);
+        }
+
+        [TestCase]
+        public void Constructor_EmptyPropertyValue_DoesNotThrow()
+        {
+            // Arrange
+            string propertyValue = string.Empty;
+
+            // Act
+            TestDelegate action = () => new AssertStateExpCommand(x => x.Title == propertyValue);
+
+            // Assert
+            Assert.DoesNotThrow(action);
+        }
+
+        [TestCase]
+        public void Execute_UninitializedWebDriver_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var command = new AssertStateExpCommand(x => x.Title == DefaultPropertyValue);
+            object webDriverObj = null;
+
+            // Act
+            TestDelegate action = () => command.Execute(webDriverObj);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [TestCase]
+        public void Execute_DifferentThanWebDriverType_ThrowsArgumentException()
+        {
+            // Arrange
+            var command = new AssertStateExpCommand(x => x.Title == DefaultPropertyValue);
+            object webDriverObj = "Not a WebDriver object";
+
+            // Act
+            TestDelegate action = () => command.Execute(webDriverObj);
+
+            // Assert
+            Assert.Throws<NullReferenceException>(action);
+        }
+    }
+}
