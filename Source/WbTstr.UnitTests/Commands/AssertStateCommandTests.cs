25d6a91b59578aad98b02f1b23495715d7696480
Onno Valkering <ovalkering@mirabeau.nl>
2017-03-21 16:49:26 +0100

    Added unit tests for AssertStateExpCommand

--- Source/WbTstr.UnitTests/Commands/AssertStateCommandTests.cs
+++ Source/WbTstr.UnitTests/Commands/AssertStateCommandTests.cs
@@ -1,11 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WbTstr.Commands;
-using WbTstr.WebDrivers;
 using WbTstr.WebDrivers.Constants;
 
 namespace WbTstr.UnitTests.Commands
