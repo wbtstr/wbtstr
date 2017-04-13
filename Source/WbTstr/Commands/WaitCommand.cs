e8110a56f903bf804d36ae5a1dac74c87d3f1495
Onno Valkering <ovalkering@mirabeau.nl>
2017-04-13 16:48:57 +0200

    Fixed typo

--- Source/WbTstr/Commands/WaitCommand.cs
+++ Source/WbTstr/Commands/WaitCommand.cs
@@ -6,17 +6,17 @@ namespace WbTstr.Commands
 {
     public class WaitCommand : IActionCommand
     {
-        private readonly int _miliseconds;
+        private readonly int _milliseconds;
         private readonly int _minutes;
         private readonly int _seconds;
 
-        public WaitCommand(int miliseconds, int seconds, int minutes)
+        public WaitCommand(int milliseconds, int seconds, int minutes)
         {
-            if (miliseconds < 0) throw new ArgumentException($"{nameof(miliseconds)} must be a positive number.");
+            if (milliseconds < 0) throw new ArgumentException($"{nameof(milliseconds)} must be a positive number.");
             if (seconds < 0) throw new ArgumentException($"{nameof(seconds)} must be a positive number.");
             if (minutes < 0) throw new ArgumentException($"{nameof(minutes)} must be a positive number.");
 
-            _miliseconds = miliseconds;
+            _milliseconds = milliseconds;
             _seconds = seconds;
             _minutes = minutes;
         }
@@ -25,12 +25,12 @@ namespace WbTstr.Commands
 
         public void Execute(object webDriverObj)
         {
-            Thread.Sleep(new TimeSpan(0, 0, _minutes, _seconds, _miliseconds));
+            Thread.Sleep(new TimeSpan(0, 0, _minutes, _seconds, _milliseconds));
         }
 
         public override string ToString()
         {
-            return $"Waiting for {_minutes}m, {_seconds}s and {_miliseconds}ms";
+            return $"Waiting for {_minutes}m, {_seconds}s and {_milliseconds}ms";
         }
     }
 }
