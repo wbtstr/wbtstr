f1da4af2d9a1d238b55e02492621ba70e45c19f2
Onno Valkering <ovalkering@mirabeau.nl>
2017-05-26 14:43:41 +0200

    Added WaitCommand tests

---
+++ Source/WbTstr.UnitTests/Commands/WaitCommandTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using WbTstr.Commands;
+using WbTstr.UnitTests._Auxiliaries;
+
+namespace WbTstr.UnitTests.Commands
+{
+    [TestFixture]
+    public class WaitCommandTests
+    {
+        private const int DefaultMilliseconds = 0;
+        private const int DefaultSeconds = 0;
+        private const int DefaultMinutes = 0;
+
+        private WaitCommand _defaultCommand;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _defaultCommand = new WaitCommand(DefaultMilliseconds, DefaultSeconds, DefaultMinutes);
+        }
+
+        [TestCase]
+        public void ToString_NoArguments_ReturnString()
+        {
+            // Arrange
+
+            // Act
+            string stringRepresentation = _defaultCommand.ToString();
+
+            // Assert
+            AssertString.NotNullOrWhiteSpace(stringRepresentation);
+        }
+
+        [TestCase]
+        public void Constructor_InvalidParams_ThrowsArgumentException()
+        {
+            // Arrange 
+            int valid = 0;
+            int invalid = -1;
+
+            // Act
+            TestDelegate[] actions =
+            {
+                () => new WaitCommand(invalid, invalid, invalid), 
+                () => new WaitCommand(invalid, valid, valid),
+                () => new WaitCommand(valid, invalid, valid),
+                () => new WaitCommand(valid, valid, invalid),
+            };
+
+            // Assert
+            AssertMultiple.Throws<ArgumentException>(actions);
+        }
+    }
+}
