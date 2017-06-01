cdbd082d0dce3934d5ae9f2a8a21d0e78bc30181
Onno Valkering <ovalkering@mirabeau.nl>
2017-06-01 16:32:58 +0200

    Added some WebDriverFactory, -State and -Utilities tests

---
+++ Source/WbTstr.UnitTests/WebDrivers/WebDriverUtilitiesTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
+using WbTstr.WebDrivers;
+
+namespace WbTstr.UnitTests.WebDrivers
+{
+    [TestFixture]
+    public class WebDriverUtilitiesTests
+    {
+        [TestCase]
+        public void WebDriverToObject_NullWebDriver_ThrowsArgumentNullException()
+        {
+            // Arrange
+            IWebDriver webDriver = null;
+
+            // Act
+            TestDelegate action = () => WebDriverUtilities.WebDriverToObject(webDriver);
+
+            // Assert  
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [TestCase]
+        public void ObjectToWebDriver_NullWebDriverObj_ThrowsArgumentException()
+        {
+            // Arrange
+            object webDriverObj = null;
+
+            // Act
+            TestDelegate action = () => WebDriverUtilities.ObjectToWebDriver(webDriverObj);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        public void WebDriverToJavaScriptExecutor_NullWebDriver_ThrowsArgumentNullException()
+        {
+            // Arrange
+            IWebDriver webDriver = null;
+
+            // Act
+            TestDelegate action = () => WebDriverUtilities.WebDriverToJavaScriptExecutor(webDriver);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+    }
+}
