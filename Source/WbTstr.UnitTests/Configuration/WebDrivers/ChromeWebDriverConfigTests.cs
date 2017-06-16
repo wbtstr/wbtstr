419909f05e0bc6954c91f69115586b394707de47
Onno Valkering <ovalkering@mirabeau.nl>
2017-06-16 13:28:56 +0200

    Added WebDriverConfig tests

---
+++ Source/WbTstr.UnitTests/Configuration/WebDrivers/ChromeWebDriverConfigTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using WbTstr.Configuration.WebDrivers;
+using WbTstr.WebDrivers.Constants;
+
+namespace WbTstr.UnitTests.Configuration.WebDrivers
+{
+    [TestFixture]
+    public class ChromeWebDriverConfigTests
+    {
+        private ChromeWebDriverConfig _defaultConfig;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _defaultConfig = new ChromeWebDriverConfig();
+        }
+
+        [TestCase]
+        public void Type_GetTypeProperty_ReturnsChromeType()
+        {
+            // Arrange
+            var expected = WebDriverType.Chrome;
+
+            // Act
+            var actual = _defaultConfig.Type;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
