b913b9e95a6f47d63a9706bd78d13996803d0940
Onno Valkering <ovalkering@mirabeau.nl>
2017-06-16 14:50:07 +0200

    Updated a few missing attributes

--- Source/WbTstr.IntegrationTests/SimpleWbTstrFixture/SimpleFixtureTests.cs
+++ Source/WbTstr.IntegrationTests/SimpleWbTstrFixture/SimpleFixtureTests.cs
@@ -11,7 +11,7 @@ namespace WbTstr.IntegrationTests.Fixtures
     [WebDriverConfig(WebDriverType.Chrome)]
     public class DerivedFromSimpleFixture : SimpleWbTstrFixture
     {
-        [Test]
+        [TestCase]
         public void TestMethod()
         {
             // Arrange
