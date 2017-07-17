9fd77e64ed411db593e44bed7938026b12ead181
Rick van Schalkwijk <rickvschalkwijk@gmail.com>
2017-07-17 13:19:18 +0200

    Clean up
    Removed unused usings, fixed some brackets and spacing, fix namespaces.

--- Source/WbTstr.UnitTests/_Auxiliaries/AssertMultiple.cs
+++ Source/WbTstr.UnitTests/_Auxiliaries/AssertMultiple.cs
@@ -1,9 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WbTstr.UnitTests._Auxiliaries
 {
--- Source/WbTstr/Fixtures/SimpleWbTstrFixture.cs
+++ Source/WbTstr/Fixtures/SimpleWbTstrFixture.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WbTstr.Session.Performers;
+﻿using WbTstr.Session.Performers;
 using WbTstr.Session.Performers.Interfaces;
 using WbTstr.Session.Recorders;
 using WbTstr.Session.Trackers;
@@ -13,11 +8,9 @@ namespace WbTstr.Fixtures
     public abstract class SimpleWbTstrFixture<P> : WbTstrFixture<SimpleSessionRecorder, P, SimpleSessionTracker>
         where P : class, ISessionPerformer, new()
     {
-
     }
 
     public abstract class SimpleWbTstrFixture: SimpleWbTstrFixture<SequentialSessionPerformer>
-    {
-                                
+    {                         
     }
 }
