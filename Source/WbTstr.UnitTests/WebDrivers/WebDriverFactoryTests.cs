100fa5c558b9e569c7a0c22a04a0f34ff90197cb
Rick van Schalkwijk <rickvschalkwijk@gmail.com>
2017-07-19 11:57:56 +0200

    added unit tests for the chrome drier options

--- Source/WbTstr.UnitTests/WebDrivers/WebDriverFactoryTests.cs
+++ Source/WbTstr.UnitTests/WebDrivers/WebDriverFactoryTests.cs
@@ -4,6 +4,8 @@ using NUnit.Framework;
 using WbTstr.Configuration.WebDrivers.Interfaces;
 using WbTstr.WebDrivers;
 using WbTstr.UnitTests._Stubs;
+using WbTstr.Configuration.WebDrivers.Options;
+using WbTstr.Configuration.WebDrivers;
 
 namespace WbTstr.UnitTests.WebDrivers
 {
