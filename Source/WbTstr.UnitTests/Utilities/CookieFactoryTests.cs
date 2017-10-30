ddddbaae6c30e82a708ea1b5eeb76a893ee598b0
Onno Valkering <ovalkering@mirabeau.nl>
2017-10-30 17:31:33 +0100

    Added more unit tests

---
+++ Source/WbTstr.UnitTests/Utilities/CookieFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WbTstr.Utilities;
+
+namespace WbTstr.UnitTests.Utilities
+{
+    [TestFixture]
+    public class CookieFactoryTests
+    {
+        [TestCase]
+        public void Create_NullParameters_DoesNotThrow()
+        {
+            // Arrange
+
+            // Act
+            TestDelegate action = () => CookieFactory.Create(null, null);
+
+            // Assert
+            Assert.DoesNotThrow(action);
+        }
+    }
+}
