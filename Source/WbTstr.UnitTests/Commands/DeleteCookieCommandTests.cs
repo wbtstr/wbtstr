afd35a0996f103a09e1c7b562117a69d3ca9c829
Onno Valkering <ovalkering@mirabeau.nl>
2017-10-30 17:59:45 +0100

    Added more unit tests

---
+++ Source/WbTstr.UnitTests/Commands/DeleteCookieCommandTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WbTstr.Commands;
+using WbTstr.UnitTests._Auxiliaries;
+
+namespace WbTstr.UnitTests.Commands
+{
+    [TestFixture]
+    public class DeleteCookieCommandTests
+    {
+        private const string DefaultName = "login";
+
+        private DeleteCookieCommand _defaultCommand;
+
+        [SetUp]
+        public void OneTimeSetUp()
+        {
+            _defaultCommand = new DeleteCookieCommand(DefaultName);
+        }
+
+        [TestCase]
+        public void ToString_NoArguments_ReturnString()
+        {
+            // Arrange
+
+            // Act
+            string stringRepresentation = _defaultCommand.ToString();
+
+            // Assert
+            AssertString.NotNullOrWhiteSpace(stringRepresentation);
+        }
+
+        [TestCase]
+        public void Constructor_NameNull_ThrowArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+            TestDelegate action = () => new DeleteCookieCommand(null);
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(action);
+        }
+    }
+}
